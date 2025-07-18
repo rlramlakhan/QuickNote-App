0f69d1c02ab2e5ef1b4fcc0117b99fc2010ea5f3
Ram Lakhan <rl4.ramlakhan@gmail.com>
2025-07-18 15:35:09 +0530

    initial commit

---
+++ app/src/main/java/com/rl/quicknote/view/activities/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.rl.quicknote.view.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.rl.quicknote.R
+import com.rl.quicknote.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}

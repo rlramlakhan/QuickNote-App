c6c4711036bbcaa23f4d93abcb09fbf4c26ee92d
Ram Lakhan <rl4.ramlakhan@gmail.com>
2025-07-19 14:23:08 +0530

    add note layout

--- app/src/main/java/com/rl/quicknote/view/fragments/HomeFragment.kt
+++ app/src/main/java/com/rl/quicknote/view/fragments/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.rl.quicknote.R
 import com.rl.quicknote.databinding.FragmentHomeBinding
 
@@ -23,6 +24,10 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.fabBtnAdd.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_noteFragment)
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/rl/quicknote/view/fragments/NoteFragment.kt
+++ app/src/main/java/com/rl/quicknote/view/fragments/NoteFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.rl.quicknote.R
 import com.rl.quicknote.databinding.FragmentNoteBinding
 
@@ -23,6 +24,10 @@ class NoteFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.ibBackNote.setOnClickListener {
+            findNavController().navigateUp()
+        }
     }
 
     override fun onDestroyView() {

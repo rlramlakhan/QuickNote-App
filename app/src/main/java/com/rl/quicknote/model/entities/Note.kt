4108d897335781fa9e0649afed7cf543c5efd1e9
Ram Lakhan <rl4.ramlakhan@gmail.com>
2025-07-19 12:53:37 +0530

    setup note database

---
+++ app/src/main/java/com/rl/quicknote/model/entities/Note.kt
@@ -0,0 +1,9 @@
+package com.rl.quicknote.model.entities
+
+data class Note(
+    val id: String,
+    val title: String,
+    val content: String,
+    val timeStamp: String,
+    val uid: String
+)
---
+++ app/src/main/java/com/rl/quicknote/model/repositories/NoteRepository.kt
@@ -0,0 +1,59 @@
+package com.rl.quicknote.model.repositories
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.rl.quicknote.model.entities.Note
+
+class NoteRepository {
+
+    private val firebaseDatabase = FirebaseDatabase.getInstance().getReference("notes")
+    private val authRepository = AuthRepository()
+
+    fun addNote(note: Note) {
+        firebaseDatabase.child(note.uid).child(note.id).setValue(note)
+    }
+
+    fun getNotes(): LiveData<List<Note>> {
+        val list = MutableLiveData<List<Note>>()
+
+        val userId = authRepository.getCurrentUser()?.uid.toString()
+        firebaseDatabase.child(userId).addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val noteList = mutableListOf<Note>()
+                if (snapshot.exists()) {
+                    for (noteSnap in snapshot.children) {
+                        val id = noteSnap.child("id").value.toString()
+                        val title = noteSnap.child("title").value.toString()
+                        val content = noteSnap.child("content").value.toString()
+                        val timeStamp = noteSnap.child("timeStamp").value.toString()
+                        val uid = noteSnap.child("uid").value.toString()
+                        val note = Note(id, title, content, timeStamp, uid)
+                        noteList.add(note)
+                    }
+                    list.value = noteList
+                } else {
+                    list.value = emptyList()
+                }
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                list.value = emptyList()
+            }
+
+        })
+        return list
+    }
+
+    fun updateNote(note: Note) {
+        firebaseDatabase.child(note.uid).child(note.id).setValue(note)
+    }
+
+    fun deleteNote(note: Note) {
+        firebaseDatabase.child(note.uid).child(note.id).removeValue()
+    }
+}
